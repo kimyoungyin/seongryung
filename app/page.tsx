ed6f6bb80e916767543dc0a0b254aee761397eb4
kimyoungyin <yin199859@gmail.com>
2024-12-09 17:56:47 +0900

    style: bigger title heading

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import InputBox from "./components/InputBox";
 export default function Page() {
     return (
         <div>
-            <h1>승룡이네집 도서 검색 시스템</h1>
+            <h1 className="text-2xl">승룡이네집 도서 검색 시스템</h1>
             <InputBox />
         </div>
     );
