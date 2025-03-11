7841072118e8af7ab8e688cb4d5cf00474e9d562
kimyoungyin <yin199859@gmail.com>
2025-03-11 16:34:28 +0900

    feat: add fallback about SearchResults

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import SearchResults from "@/app/components/SearchResults";
 import InputBox from "./components/InputBox";
+import { Suspense } from "react";
+
+const fallback = <div>로딩 중...</div>;
 
 export default async function Page(props: {
     searchParams?: Promise<{
@@ -13,7 +16,9 @@ export default async function Page(props: {
         <div>
             <h1 className="text-2xl">승룡이네집 도서 검색 시스템</h1>
             <InputBox />
-            <SearchResults query={query} />
+            <Suspense fallback={fallback}>
+                <SearchResults query={query} />
+            </Suspense>
         </div>
     );
 }
