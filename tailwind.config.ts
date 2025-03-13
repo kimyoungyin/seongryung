7f0924b0d5a10085d3f6ba2fe53294918c4c6211
kimyoungyin <yin199859@gmail.com>
2025-03-13 16:37:08 +0900

    styles: tailwind.config.ts 파일을 이용해 테마 색 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        colors: {
+            "base-bg": "#fff8e3", // 전체 배경
+            "button-bg": "#d4b88a", // 버튼 배경
+            "card-bg": "#fffdf8", // 카드 배경
+            "input-border": "#e0d6c2", // Input border
+            "text-primary": "#5d5348", // primary
+            "text-secondary": "#726a5f", // secondary
+        },
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
