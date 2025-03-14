b2217865578555341286593938b5a0306069bc8b
kimyoungyin <yin199859@gmail.com>
2025-03-14 15:21:10 +0900

    refactor: 검색 결과 없을 때 안내 상세히 작성

--- app/components/SearchResults.tsx
+++ app/components/SearchResults.tsx
@@ -16,7 +16,7 @@ export default async function SearchResults({ query }: { query: string }) {
                 <div className="max-w-2xl text-center space-y-8">
                     {/* 아이콘 그룹 */}
                     <div className="mx-auto w-fit">
-                        <SearchX className="w-32 h-32 text-text-primary animate-float" />
+                        <SearchX className="w-24 h-24 sm:w-32 sm:h-32 text-text-primary animate-float" />
                     </div>
 
                     {/* 텍스트 영역 */}
@@ -31,10 +31,27 @@ export default async function SearchResults({ query }: { query: string }) {
                         </span>
 
                         <div className="py-4 px-6 bg-skeleton/30 rounded-lg inline-block">
-                            <p className="text-sm sm:text-base text-text-primary/80">
-                                ✓ 책 제목의 일부만 입력해보세요
-                                <br />✓ 띄어쓰기를 정확히 해보세요
-                            </p>
+                            <div className="text-sm sm:text-base text-text-primary/80 flex flex-col gap-4">
+                                <p className="flex flex-col">
+                                    ✓ 책 제목의 일부만 입력해보세요
+                                    <span className="text-xs">
+                                        ("그대를 사랑합니다" -&gt; "그대를")
+                                    </span>
+                                </p>
+                                <p className="flex flex-col">
+                                    ✓ 띄어쓰기를 정확히 해보세요.
+                                    <span className="text-xs">
+                                        ("그대를사랑합니다" -&gt; "그대를
+                                        사랑합니다")
+                                    </span>
+                                </p>
+                                <p className="flex flex-col">
+                                    ✓ 영어 제목은 한글로 변경해보세요
+                                    <span className="text-xs">
+                                        (Marvel -&gt; 마블)
+                                    </span>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
