61860ecfe9d56f0d5d14407802ca8c48b4f23009
kimyoungyin <yin199859@gmail.com>
2025-03-14 16:54:18 +0900

    refactor: SearchResults에 나오는 각각의 카드들을 detail 페이지에 재사용하기 위해 BookCard로 분리

--- app/components/SearchResults.tsx
+++ app/components/SearchResults.tsx
@@ -1,8 +1,6 @@
+import BookCard from "@/app/components/BookCard";
 import { search } from "@/app/utils/actions";
-import { getImageSrc } from "@/app/utils/util";
-import { BookOpenCheck, SearchX } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import { SearchX } from "lucide-react";
 
 export default async function SearchResults({ query }: { query: string }) {
     // 첫 렌더링 시에 전체 검색 불러오지 않도록 수정
@@ -61,54 +59,11 @@ export default async function SearchResults({ query }: { query: string }) {
     return (
         <>
             {books.map((bookObj) => (
-                <div
+                <BookCard
                     key={bookObj.id}
-                    className="bg-card-bg rounded-xl p-6 shadow-md hover:shadow-lg transition-shadow cursor-pointer"
-                >
-                    <div className="flex gap-6">
-                        <div className="relative sm:w-24 sm:h-36 w-16 h-24 self-center">
-                            <Image
-                                src={getImageSrc(bookObj.location, bookObj.id)}
-                                alt={bookObj.title}
-                                fill
-                                sizes="(min-width: 640px) 6rem, 4rem" // w-24, w-16
-                                className="object-cover rounded-lg"
-                                quality={80}
-                            />
-                        </div>
-                        <div className="flex-1 flex flex-col sm:flex-row justify-between gap-4">
-                            <div>
-                                <h2 className="text-lg md:text-xl lg:text-2xl font-bold mb-2 text-text-primary">
-                                    {bookObj.title}
-                                </h2>
-                                <div className="space-y-1 flex flex-col gap-1 text-text-secondary text-md md:text-lg lg:text-xl">
-                                    {bookObj.author && (
-                                        <p>작가: {bookObj.author}</p>
-                                    )}
-                                    {bookObj.season && (
-                                        <p>시즌: {bookObj.season}기</p>
-                                    )}
-                                    {bookObj.total_num && (
-                                        <p>총 권수: {bookObj.total_num}권</p>
-                                    )}
-                                    {bookObj.comment && (
-                                        <p className="mt-3 text-text-primary">
-                                            {bookObj.comment}
-                                        </p>
-                                    )}
-                                </div>
-                            </div>
-                            <div className="self-end sm:self-center text-xs md:text-sm lg:text-md">
-                                <Link
-                                    href={`/detail/${bookObj.id}`}
-                                    className="bg-[#e8e0d5] text-text-primary px-4 py-2 rounded-md hover:bg-[#dcd4c9] transition-colors whitespace-nowrap"
-                                >
-                                    위치 보기 →
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    bookObj={bookObj}
+                    isAboutLocation={false}
+                />
             ))}
         </>
     );
