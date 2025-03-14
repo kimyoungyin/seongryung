5b36cc8c1667287d063c49c56e53d79a005945d7
kimyoungyin <yin199859@gmail.com>
2025-03-14 16:55:24 +0900

    refactor: Book 하나에 대한 타입인데 Books, 즉 복수형 처리했던 것을 수정

--- app/utils/actions.ts
+++ app/utils/actions.ts
@@ -1,5 +1,5 @@
 "use server";
-import { Books, queryDatabase } from "@/app/utils/db";
+import { Book, queryDatabase } from "@/app/utils/db";
 // https://nextjs-ko.org/docs/app/building-your-application/data-fetching/fetching#orms-and-database-clients
 import { cache } from "react";
 
@@ -7,11 +7,11 @@ export const search = cache(async (bookName: string) => {
     const pureBookName = bookName.trim();
     const sql = `SELECT * FROM books WHERE title LIKE ?`;
     // SELECT * FROM books WHERE title LIKE %?% 대신 다음과 같이 포함 검색
-    return (await queryDatabase(sql, [`%${pureBookName}%`])) as Books[];
+    return (await queryDatabase(sql, [`%${pureBookName}%`])) as Book[];
 });
 
 export const getBookDetail = cache(async (bookId: number) => {
     const sql = `SELECT * FROM books WHERE id = ? LIMIT 1`;
     // SELECT * FROM books WHERE title LIKE %?% 대신 다음과 같이 포함 검색
-    return (await queryDatabase(sql, [bookId])) as [Books];
+    return (await queryDatabase(sql, [bookId])) as [Book];
 });
