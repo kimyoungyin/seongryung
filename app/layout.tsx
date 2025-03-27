7e10789d8f070387eba25da0d74ab0b510313371
kimyoungyin <yin199859@gmail.com>
2025-03-27 17:10:46 +0900

    feat: Header에 Home으로 가는 링크 연결

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -26,25 +27,30 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <div className="min-h-screen bg-base-bg">
-                    <header className="w-full bg-base-bg px-8 pt-6 pb-3 flex justify-center">
-                        <div className="flex-1 max-w-4xl flex items-center h-12 sm:h-16 gap-3">
-                            {/* 이미지 영역 (테마 색상 적용) */}
-                            <div className="w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-button-bg/20 border-2 border-input-border">
-                                {/* 여기 캐릭터 이미지 올려야  */}
-                                <Image
-                                    src={"/seongryoung-sm-round.jpeg"}
-                                    alt="승룡이네집"
-                                    width={32}
-                                    height={32}
-                                    className="w-full h-full bg-contain"
-                                />
-                            </div>
+                    <header>
+                        <Link
+                            href={"/"}
+                            className="w-full bg-base-bg px-4 pt-3 pb-1 sm:px-8 sm:pt-6 sm:pb-3 flex justify-center"
+                        >
+                            <div className="justify-center sm:justify-start flex-1 max-w-4xl flex items-center h-12 sm:h-16 gap-3">
+                                {/* 이미지 영역 (테마 색상 적용) */}
+                                <div className="w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-button-bg/20 border-2 border-input-border">
+                                    {/* 여기 캐릭터 이미지 올려야  */}
+                                    <Image
+                                        src={"/seongryoung-sm-round.jpeg"}
+                                        alt="승룡이네집"
+                                        width={32}
+                                        height={32}
+                                        className="w-full h-full bg-contain"
+                                    />
+                                </div>
 
-                            {/* 텍스트 계층 구조 개선 */}
-                            <h1 className="text-[min(calc(((100vw-128px))/13),30px)] font-extrabold text-text-primary">
-                                승룡이네집 도서 검색 시스템
-                            </h1>
-                        </div>
+                                {/* 텍스트 계층 구조 개선 */}
+                                <h1 className="text-[min(calc(((100vw-128px))/13),30px)] font-extrabold text-text-primary">
+                                    승룡이네집 도서 검색 시스템
+                                </h1>
+                            </div>
+                        </Link>
                     </header>
                     <div className="max-w-4xl mx-auto p-4 sm:p-6">
                         {children}
