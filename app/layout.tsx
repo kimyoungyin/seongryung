946ffe31c8d33444423f7fb374f7526af50d2984
kimyoungyin <yin199859@gmail.com>
2025-03-28 10:56:59 +0900

    refactor: Header에 링크 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Image from "next/image";
-import Link from "next/link";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -27,30 +26,25 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <div className="min-h-screen bg-base-bg">
-                    <header>
-                        <Link
-                            href={"/"}
-                            className="w-full bg-base-bg px-4 pt-3 pb-1 sm:px-8 sm:pt-6 sm:pb-3 flex justify-center"
-                        >
-                            <div className="justify-center sm:justify-start flex-1 max-w-4xl flex items-center h-12 sm:h-16 gap-3">
-                                {/* 이미지 영역 (테마 색상 적용) */}
-                                <div className="w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-button-bg/20 border-2 border-input-border">
-                                    {/* 여기 캐릭터 이미지 올려야  */}
-                                    <Image
-                                        src={"/seongryoung-sm-round.jpeg"}
-                                        alt="승룡이네집"
-                                        width={32}
-                                        height={32}
-                                        className="w-full h-full bg-contain"
-                                    />
-                                </div>
-
-                                {/* 텍스트 계층 구조 개선 */}
-                                <h1 className="text-[min(calc(((100vw-128px))/13),30px)] font-extrabold text-text-primary">
-                                    승룡이네집 도서 검색 시스템
-                                </h1>
+                    <header className="w-full bg-base-bg px-4 pt-3 pb-1 sm:px-8 sm:pt-6 sm:pb-3 flex justify-center">
+                        <div className="justify-center sm:justify-start flex-1 max-w-4xl flex items-center h-12 sm:h-16 gap-3">
+                            {/* 이미지 영역 (테마 색상 적용) */}
+                            <div className="w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-button-bg/20 border-2 border-input-border">
+                                {/* 여기 캐릭터 이미지 올려야  */}
+                                <Image
+                                    src={"/seongryoung-sm-round.jpeg"}
+                                    alt="승룡이네집"
+                                    width={32}
+                                    height={32}
+                                    className="w-full h-full bg-contain"
+                                />
                             </div>
-                        </Link>
+
+                            {/* 텍스트 계층 구조 개선 */}
+                            <h1 className="text-[min(calc(((100vw-128px))/13),30px)] font-extrabold text-text-primary">
+                                승룡이네집 도서 검색 시스템
+                            </h1>
+                        </div>
                     </header>
                     <div className="max-w-4xl mx-auto p-4 sm:p-6">
                         {children}
