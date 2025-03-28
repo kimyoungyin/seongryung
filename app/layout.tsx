b1b54f5dff571400e93a1f0e0db97cec438d4185
kimyoungyin <yin199859@gmail.com>
2025-03-28 11:57:42 +0900

    feat: vercel analytics와 speedInsights 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Image from "next/image";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -49,6 +51,8 @@ export default function RootLayout({
                     <div className="max-w-4xl mx-auto p-4 sm:p-6">
                         {children}
                         {locationModal}
+                        <Analytics />
+                        <SpeedInsights />
                     </div>
                 </div>
             </body>
