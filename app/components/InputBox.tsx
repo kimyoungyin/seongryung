0baed4510bf493bf674ebc65ab6420c013105f28
kimyoungyin <yin199859@gmail.com>
2025-04-03 14:51:08 +0900

    refactor: 키워드 검색, Input 검색 시 로딩 UI 표시

--- app/components/InputBox.tsx
+++ app/components/InputBox.tsx
@@ -1,20 +1,28 @@
 "use client";
 
+import { Loader2 } from "lucide-react";
 import InitialNotification from "@/app/components/InitialNotification";
 import { Search } from "lucide-react";
-// import SearchResults from "@/app/components/SearchResults";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { KeyboardEvent } from "react";
 
 const QUERY = "query";
 
 export default function InputBox() {
+    const [isSearching, setIsSearching] = useState(false);
+    const [searchingKeyword, setSearchingKeyword] = useState(null);
+    // 현재 검색 쿼리
     const searchParams = useSearchParams();
-    // handleSearchParamsChange 뿐만 아니라, 다른 핸들러에도 사용해야 하는데,
-    // 여기서 사용하는 리렌더링 관련 훅이 없으므로 최상단에 노출시킴
+    // 검색할 새로운 쿼리
     const params = new URLSearchParams(searchParams);
     const pathname = usePathname();
     const { push } = useRouter();
+
+    useEffect(() => {
+        setIsSearching(false);
+        setSearchingKeyword(null);
+    }, [searchParams]);
 
     const handleSearchParamsChange = (term: string) => {
         const trimmedTerm = term.trim();
@@ -26,7 +34,13 @@ export default function InputBox() {
     };
 
     const handleSubmit = () => {
-        if (params.get(QUERY)) push(`${pathname}?${params.toString()}`);
+        const currentQuery = searchParams.get(QUERY);
+        const newQuery = params.get(QUERY);
+        // 검색어가 있고, 현재 검색어와 다를 때
+        if (newQuery && newQuery !== currentQuery) {
+            setIsSearching(true);
+            push(`${pathname}?${params.toString()}`);
+        }
     };
 
     const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
@@ -34,8 +48,14 @@ export default function InputBox() {
     };
 
     const onKeywordClick = (keyword: string) => {
-        params.set(QUERY, keyword.trim());
-        push(`${pathname}?${params.toString()}`);
+        const currentQuery = searchParams.get(QUERY);
+        const newQuery = keyword.trim();
+        // 검색어가 있고, 현재 검색어와 다를 때
+        if (newQuery !== currentQuery) {
+            params.set(QUERY, newQuery);
+            setSearchingKeyword(keyword);
+            push(`${pathname}?${params.toString()}`);
+        }
     };
 
     return (
@@ -55,27 +75,54 @@ export default function InputBox() {
                     />
                     <button
                         onClick={handleSubmit}
-                        className="bg-button-bg text-text-primary px-4 sm:px-6 py-2 sm:py-3  rounded-lg hover:bg-button-bg-hover transition-colors flex items-center gap-2"
+                        disabled={isSearching}
+                        aria-label="검색"
+                        aria-disabled={isSearching}
+                        className="bg-button-bg text-text-primary px-4 sm:px-6 py-2 sm:py-3  rounded-lg hover:bg-button-bg-hover transition-colors flex items-center gap-2 disabled:cursor-not-allowed disabled:opacity-50"
                     >
-                        <Search className="w-[min(calc(((100vw-128px))/20),16px)] h-[min(calc(((100vw-128px))/20),16px)]" />
-                        검색
+                        {isSearching ? (
+                            <>
+                                <Loader2 className="h-4 w-4 animate-spin" />
+                                <span>위치 찾는 중...</span>
+                            </>
+                        ) : (
+                            <>
+                                <Search className="w-[min(calc(((100vw-128px))/20),16px)] h-[min(calc(((100vw-128px))/20),16px)]" />
+                                검색
+                            </>
+                        )}
                     </button>
                 </div>
                 <div className="ml-3 flex items-center gap-2 text-muted-foreground text-text-secondary text-[min(calc(((100vw-84px-1.5rem))/24),14px)]">
                     추천 검색어:
                     <div className="flex flex-wrap gap-3">
                         {["바보", "그대를 사랑합니다", "무빙"].map(
-                            (keyword) => (
-                                <button
-                                    key={keyword}
-                                    onClick={() => onKeywordClick(keyword)}
-                                    className="px-2 py-1 sm:px-4 sm:py-2 bg-button-bg/10 text-text-primary bg-button-bg
-						rounded-full hover:bg-text-primary/20 transition-colors
-						active:ring-2 active:ring-text-primary/50 active:bg-text-primary/50"
-                                >
-                                    {keyword}
-                                </button>
-                            )
+                            (keyword) => {
+                                const isKeywordSearching =
+                                    searchingKeyword === keyword;
+                                return (
+                                    <button
+                                        key={keyword}
+                                        onClick={() => onKeywordClick(keyword)}
+                                        disabled={isKeywordSearching}
+                                        aria-label={`${keyword} 검색`}
+                                        aria-disabled={isKeywordSearching}
+                                        className="px-2 py-1 sm:px-4 sm:py-2 bg-button-bg/10 text-text-primary bg-button-bg
+						rounded-full flex gap-2 items-center hover:bg-text-primary/20 transition-colors
+						active:ring-2 active:ring-text-primary/50 active:bg-text-primary/50
+						disabled:cursor-not-allowed disabled:opacity-50"
+                                    >
+                                        {isKeywordSearching ? (
+                                            <>
+                                                <Loader2 className="h-4 w-4 animate-spin" />
+                                                <span>검색 중...</span>
+                                            </>
+                                        ) : (
+                                            keyword
+                                        )}
+                                    </button>
+                                );
+                            }
                         )}
                     </div>
                 </div>
